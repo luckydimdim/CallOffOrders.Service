36fdf1fa215a91e0693503c6947cfa98a04d4e28
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-06-23 10:53:11 +0300

    round fix

--- CallOffOrders.Service/AutoMapperProfile.cs
+++ CallOffOrders.Service/AutoMapperProfile.cs
@@ -8,20 +8,32 @@ namespace Cmas.Services.CallOffOrders
 {
     public class AutoMapperProfile : Profile
     {
+        /// <summary>
+        /// Округление до двух знаков после запятой
+        /// Округление происходит к тому числу, которое дальше от нуля
+        /// </summary>
+        public double RoundDoubleTwo(double value)
+        {
+            return Math.Round(value, 2, MidpointRounding.AwayFromZero);
+        }
+
         public AutoMapperProfile()
         {
-            CreateMap<UpdateRateRequest, Rate>();
-            CreateMap<RateRequest, Rate>();
+            CreateMap<UpdateRateRequest, Rate>()
+                .ForMember(item => item.Amount, opt => opt.ResolveUsing(i => RoundDoubleTwo(i.Amount)));
+
+            CreateMap<RateRequest, Rate>()
+                .ForMember(item => item.Amount, opt => opt.ResolveUsing(i => RoundDoubleTwo(i.Amount)));
+
             CreateMap<UpdateCallOffOrderRequest, CallOffOrder>();
             CreateMap<CreateCallOffOrderRequest, CallOffOrder>();
             CreateMap<CallOffOrder, DetailedCallOffOrderResponse>();
             CreateMap<CallOffOrder, SimpleCallOffOrderResponse>();
             CreateMap<Rate, RateResponse>();
 
-            CreateMap<RateUnit, int>().ConvertUsing(src => (int)src);
+            CreateMap<RateUnit, int>().ConvertUsing(src => (int) src);
             CreateMap<int, RateUnit>()
-                .ConvertUsing(src => (RateUnit)Enum.Parse(typeof(RateUnit), src.ToString()));
+                .ConvertUsing(src => (RateUnit) Enum.Parse(typeof(RateUnit), src.ToString()));
         }
     }
-
 }
